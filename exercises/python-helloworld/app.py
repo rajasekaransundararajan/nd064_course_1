f041ee1bcc1f84f4845c418eaaef7de92848463f
Rajasekaran Sundararajan <rajasekaran.sundararajan@philips.com>
2022-11-07 19:28:28 +0530

    Modified files

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,35 @@
 from flask import Flask
+import logging
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    app.logger.info('Main request successfull')
+    return "Hello 2 World!"
+
+@app.route('/status')
+def status():
+    app.logger.info('status request successfull')
+    response = app.response_class(
+          response=json.dumps({"result":"OK - healthy"}),
+          status=200,
+          mimetype='application/json'
+          )
+    return response
+
+@app.route('/metrics')
+def metrics():
+    app.logger.info('Main request successfull')
+    response = app.response_class(
+          response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+          status=200,
+          mimetype='application/json'
+          )
+    return response
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    # Stream logs to a file, and set the default log level to DEBUG
+  logging.basicConfig(filename='app.log',level=logging.DEBUG)
+  app.run(host='0.0.0.0')
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,10 @@
 import sqlite3
+import logging
+from datetime import datetime
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -17,17 +20,37 @@ def get_post(post_id):
                         (post_id,)).fetchone()
     connection.close()
     return post
+access_count = 0
+def add_access_count():
+    global access_count
+    access_count = access_count + 1
+    return access_count
+
+# Function to get a count of posts 
+def get_count_of_posts():
+    connection = get_db_connection()
+    postCount = connection.execute('SELECT Count(*) FROM posts').fetchone()
+    connection.close()
+    return postCount
+    
+#Function to get current time
+def get_current_time():
+    ct = datetime.now()
+    dt_string = ct.strftime("%d/%m/%Y %H:%M:%S")
+    return dt_string
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
 # Define the main route of the web application 
+
 @app.route('/')
+
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
+    connection.close()    
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -35,14 +58,19 @@ def index():
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
+    add_access_count()
     if post is None:
+      app.logger.info(get_current_time() +' A non-existing article is accessed and a 404 page is returned')        
       return render_template('404.html'), 404
     else:
+      
+      app.logger.info(get_current_time() + ' Article ' + post["title"] + ' retrieved!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info(get_current_time() +' About us page is retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -51,10 +79,12 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        
 
         if not title:
             flash('Title is required!')
         else:
+            app.logger.info(get_current_time() +' New article is created with title :' + title)
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
@@ -65,6 +95,33 @@ def create():
 
     return render_template('create.html')
 
+
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info(get_current_time() +' Healthz request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+                   
+            postCount = get_count_of_posts()
+            response = app.response_class(
+            response=json.dumps({"db_connection_count":access_count,"post_count":postCount[0]}),
+            status=200,
+            mimetype='application/json'
+            )
+            app.logger.info(get_current_time() +' Metrics request successfull : db_connection_count ' + str(access_count) + ' post_count ' + str(postCount[0]))
+            return response
+            
 # start the application on port 3111
 if __name__ == "__main__":
+# Stream logs to a file, and set the default log level to DEBUG
+   logging.basicConfig(level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111')
+   
+
